be2758c9737722d266185af0be7fda37e0080b64
Mindy D Tran <mindytran98@gmail.com>
2024-01-26 13:42:23 -0500

    add props and classname

--- src/components/Learner.js
+++ src/components/Learner.js
@@ -1,13 +1,12 @@
-import { learnerData } from "../data/learnerData";
 import Score from "./Score";
 
-export default function Learner() {
+export default function Learner({learners}) {
   return (
     <main>
-      {learnerData.map((learner, i) => (
-        <section key={i}>
+      {learners.map((learner, i) => (
+        <section key={i} className="leaner">
           <h2> {learner.name}</h2>
-          <p>Bio: {learner.bio}</p>
+          <p><span className="bold">Bio: </span>{learner.bio}</p>
           <Score scores={learner.scores}/>
         </section>
       ))}
