23b38dcd8ade3d54c5bd8cfafeaf76f7d3c2f4a9
Mindy D Tran <mindytran98@gmail.com>
2024-01-26 13:40:23 -0500

    import useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-// import {learnerData} from './data/learnerData';
-// import useState
+import { learnerData } from "./data/learnerData";
+import { useState } from "react";
 
-import './App.css';
-import Learner from './components/Learner';
+import "./App.css";
+import Learner from "./components/Learner";
 
 function App() {
   //create a new state passing the learnerData
-  // userState(learnerData)
+  const [learners, setLearners] = useState(learnerData);
   return (
     <div className="App">
       <h1>Learner App</h1>
-      <Learner />
+      <Learner learners={learners}/>
     </div>
   );
 }
