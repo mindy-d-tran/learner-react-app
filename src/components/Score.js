c988ade82105c82cab830ccc13798626434dadac
Mindy D Tran <mindytran98@gmail.com>
2024-01-26 13:43:01 -0500

    add div to contain scores' date and score

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,12 +1,20 @@
 export default function Score(prop) {
   return (
-    <section>
-      <h3>Scores</h3>
+    <section className="score">
+      <h3>Scores: </h3>
       {prop.scores.map((score, i) => (
-        <ul key={i}>
-          <li>Date: {score.date}</li>
-          <li>Score: {score.score}</li>
-        </ul>
+        <div>
+          <ul key={i}>
+            <li>
+              <span className="bold">Date: </span>
+              {score.date}
+            </li>
+            <li>
+              <span className="bold">Score: </span>
+              {score.score}
+            </li>
+          </ul>
+        </div>
       ))}
     </section>
   );
