fcb3d3d0ca0862035f0b6b3df5eb7048804203af
Mindy D Tran <mindytran98@gmail.com>
2024-01-26 14:49:31 -0500

    add sections for each part of learner's profile

--- src/components/Learner.js
+++ src/components/Learner.js
@@ -1,14 +1,19 @@
 import Score from "./Score";
 
-export default function Learner({learners}) {
+export default function Learner({ learners }) {
   return (
     <main>
       {learners.map((learner, i) => (
-        <section key={i} className="leaner">
+        <section key={i} className="learner">
           <h2> {learner.name}</h2>
-          <p><span className="bold">Bio: </span>{learner.bio}</p>
-          <h3>Scores: </h3>
-          <Score scores={learner.scores} id={i}/>
+          <section className="bio">
+            <h3 className="bold">Bio: </h3>
+            <p>{learner.bio}</p>
+          </section>
+          <section className="score">
+            <h3>Scores: </h3>
+            <Score scores={learner.scores} id={i} />
+          </section>
         </section>
       ))}
     </main>
